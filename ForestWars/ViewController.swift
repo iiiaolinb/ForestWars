150d3bb681fc70b743543ebaeb683b37f5fe5b01
Khudyaev Egor <ekhudiaev@emphasoft.com>
2025-09-30 10:47:13 +0300

    Initial Commit

---
+++ ForestWars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForestWars
+//
+//  Created by Егор Худяев on 30.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
